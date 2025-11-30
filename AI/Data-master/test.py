3e81ad511ce651f4ce4eb7824f08f8acf53c7770
Ngô Phúc Khang <ngophuckhang2024@gmail.com>
2025-11-30 15:44:30 +0700

    Delete AI/Data-master directory

--- AI/Data-master/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-res_df = pd.read_csv(r"C:\python\Projects\TOURISM\AI\Data-master\restaurants_100_improved.csv")
-
-res_df = res_df.drop(['price_range', 'cuisine'], axis=1)
-res_df.to_csv()
-print(res_df.head().to_string())
